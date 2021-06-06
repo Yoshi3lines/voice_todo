62877d739efac501096d41dada9bcf22bcd598cc
Yoshi3lines <ny3line@gmail.com>
2021-06-06 11:15:42 +0900

    リスト機能を実装
    indexページと新規作成ページを作成
    アプリの最大値を変更した

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   
   root 'homes#index'
   
+  # デバイス関連
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
+  # リスト関連
+  resources :list, only: %i(new create index)
 end
