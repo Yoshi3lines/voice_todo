5115af32be448d0358de41eee2eeb85956d4696b
Yoshi3lines <ny3line@gmail.com>
2021-06-13 12:29:18 +0900

    carrierwaveとcloudinaryをインストール 他にも変更あり index new migration ストロングパラメータ

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -42,7 +42,7 @@ class ListController < ApplicationController
 
   private
     def list_params
-      params.require(:list).permit(:title).merge(user: current_user)
+      params.require(:list).permit(:title, :audio).merge(user: current_user)
     end
 
     def set_list
